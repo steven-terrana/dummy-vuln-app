adaca0fbdb43c514f4177ec852767c125ba95c01
steven-terrana <steventerrana@gmail.com>
2020-04-03 14:52:42 -0400

    add requirements.txt and unit test

---
+++ test_app.py
@@ -0,0 +1,8 @@
+# test_hello.py
+from app import app
+
+def test_hello_world():
+    response = app.test_client().get('/')
+
+    assert response.status_code == 200
+    assert response.data == b'Flask web server in Dockerrr!'
