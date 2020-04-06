1775a6be00c31932df8ceed66d116ee89b17d165
steven-terrana <steventerrana@gmail.com>
2020-04-06 09:28:31 -0400

    fix test failure

--- test_app.py
+++ test_app.py
@@ -5,4 +5,4 @@ def test_hello_world():
     response = app.test_client().get('/')
 
     assert response.status_code == 200
-    assert response.data == b'Flask web server in Dockerrr!'
+    assert response.data == b'Flask web server in Docker!'
