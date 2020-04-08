83c94721e0d7b05e0f9c673568d192db2f2d0202
steven-terrana <steventerrana@gmail.com>
2020-04-08 16:31:19 -0400

    change message for new image hash

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask web server in Docker!'
+    return 'Flask web server in Docker'
 
 if __name__ == '__main__':
     app.run(debug=False,host='0.0.0.0', port=5000)
