bef5ebf34a1883cf87524e2c4ee43da41d735d95
steven-terrana <steventerrana@gmail.com>
2020-04-10 15:44:32 -0400

    remove py files from root

--- test_app.py
+++
@@ -1,8 +0,0 @@
-# test_hello.py
-from app import app
-
-def test_hello_world():
-    response = app.test_client().get('/')
-
-    assert response.status_code == 200
-    assert response.data == b'Flask web server in Docker'
